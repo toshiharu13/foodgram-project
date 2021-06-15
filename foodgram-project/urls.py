588ee1f551309703e21f5ecabda4119671e0f884
elf <elorian@yandex.ru>
2021-06-15 23:03:43 +0300

    try2

---
+++ foodgram-project/urls.py
@@ -0,0 +1,38 @@
+"""foodgram-project URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include
+from django.contrib.flatpages.views import flatpage
+
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    #регистрация и авторизация
+    path('auth/', include('users.urls')),
+
+    #если нужного шаблона для /auth не нашлось в файле users.urls —
+    #ищем совпадения в файле django.contrib.auth.urls
+    path('auth/', include('django.contrib.auth.urls')),
+    path('admin/', admin.site.urls),
+    path('', include('prod_h.urls')),
+    path('about-author/', include('django.contrib.flatpages.urls')),
+    path('about-tech/', include('django.contrib.flatpages.urls')),
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
