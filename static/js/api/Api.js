1198242c012ca75112c0ab9c84c699a40e881bd0
Boyko Alexey <elorian@yandex.ru>
2021-06-24 16:10:54 +0300

    small correctings

--- static/js/api/Api.js
+++ static/js/api/Api.js
@@ -24,7 +24,7 @@ class Api {
         'X-CSRFToken': document.getElementsByName('csrfmiddlewaretoken')[0].value
       },
       body: JSON.stringify({
-        id: id
+        item: id
       })
     })
       .then( e => {
