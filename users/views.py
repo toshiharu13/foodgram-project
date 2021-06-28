343212f5ba5d687767d4288d71796ff6b59989f3
Boyko Alexey <elorian@yandex.ru>
2021-06-28 11:37:03 +0300

    move reg.html

--- users/views.py
+++ users/views.py
@@ -8,4 +8,4 @@ class SignUp(CreateView):
     form_class = CreationForm
     #  где login — это параметр "name" в path()
     success_url = reverse_lazy('login')
-    template_name = "reg.html"
+    template_name = "users/reg.html"
